8bbd062ea175775acdd62daeb5927b4a3a0a58b2
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2020-06-16 18:08:34 +0300

    fixed tasks 1 and 7

--- lab5.sql
+++ lab5.sql
@@ -1,10 +1,10 @@
 --1. Добавить внешние ключи
-ALTER TABLE [order] ADD FOREIGN KEY (id_production) REFERENCES production (id_production)
-ALTER TABLE [order] ADD FOREIGN KEY (id_dealer) REFERENCES dealer (id_dealer)
-ALTER TABLE [order] ADD FOREIGN KEY (id_pharmacy) REFERENCES pharmacy (id_pharmacy)
-ALTER TABLE dealer ADD FOREIGN KEY (id_company) REFERENCES company (id_company)
-ALTER TABLE production ADD FOREIGN KEY (id_company) REFERENCES company (id_company)
-ALTER TABLE production ADD FOREIGN KEY (id_medicine) REFERENCES medicine (id_medicine)
+ALTER TABLE [order] ADD CONSTRAINT FK_order_id_production FOREIGN KEY (id_production) REFERENCES production (id_production)
+ALTER TABLE [order] ADD CONSTRAINT FK_order_id_dealer FOREIGN KEY (id_dealer) REFERENCES dealer (id_dealer)
+ALTER TABLE [order] ADD CONSTRAINT FK_order_id_pharmacy FOREIGN KEY (id_pharmacy) REFERENCES pharmacy (id_pharmacy)
+ALTER TABLE dealer ADD CONSTRAINT FK_dealer_id_company FOREIGN KEY (id_company) REFERENCES company (id_company)
+ALTER TABLE production ADD CONSTRAINT FK_production_id_company FOREIGN KEY (id_company) REFERENCES company (id_company)
+ALTER TABLE production ADD CONSTRAINT FK_production_id_medicine FOREIGN KEY (id_medicine) REFERENCES medicine (id_medicine)
 
 -- 2.Выдать информацию по всем заказам лекарства “Кордерон” компании “Аргус” с указанием названий аптек, дат, объема заказов
 SELECT pharmacy.name, [order].date, [order].quantity FROM [order]
@@ -45,43 +45,31 @@ LEFT JOIN medicine ON medicine.id_medicine = production.id_medicine
 WHERE production.price > 3000 AND medicine.cure_duration <= 7) 
 
 --7. Добавить необходимые индексы.
-CREATE NONCLUSTERED INDEX [IX_production_id_company] ON production
+CREATE NONCLUSTERED INDEX [IX_production_id_company] ON production -- используется в заданиях 3, 4
 (
 	id_company ASC
 ) 
-CREATE NONCLUSTERED INDEX [IX_production_id_medicine] ON production
+CREATE NONCLUSTERED INDEX [IX_production_id_medicine] ON production -- используется в задании 2
 (
 	id_medicine ASC
 ) 
-CREATE NONCLUSTERED INDEX [IX_order_id_production] ON [order]
+CREATE NONCLUSTERED INDEX [IX_order_id_production] ON [order] -- используется в заданиях 2, 3, 4
 (
 	id_production ASC
 ) 
-CREATE NONCLUSTERED INDEX [IX_order_id_dealer] ON [order]
+CREATE NONCLUSTERED INDEX [IX_order_id_dealer] ON [order] -- используется в задании 5
 (
 	id_dealer ASC
 )
-CREATE NONCLUSTERED INDEX [IX_order_id_pharmacy] ON [order]
-(
-	id_pharmacy ASC
-)
-CREATE NONCLUSTERED INDEX [IX_dealer_id_company] ON dealer
+CREATE NONCLUSTERED INDEX [IX_dealer_id_company] ON dealer -- используется в задании 5
 (
 	id_company ASC
 )
-CREATE NONCLUSTERED INDEX [IX_medicine_name] ON medicine
+CREATE NONCLUSTERED INDEX [IX_medicine_name] ON medicine -- используется в задании 2
 (
 	name ASC
 )
-CREATE NONCLUSTERED INDEX [IX_company_name] ON company
+CREATE NONCLUSTERED INDEX [IX_company_name] ON company -- используется в заданиях 2, 3, 4, 5
 (
 	name ASC
-)
-CREATE NONCLUSTERED INDEX [IX_order_date] ON [order]
-(
-	date ASC
-)
-CREATE NONCLUSTERED INDEX [IX_medicine_cure_duration] ON [medicine]
-(
-	cure_duration ASC
 )
